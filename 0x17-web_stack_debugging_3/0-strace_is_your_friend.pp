0db42fa829bdab00cd4acb3cf77e0c7344fbdb50
ilyes01 <ilyesghrab7@gmail.com>
2021-11-05 11:09:08 +0100

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixing file not found
+exec{ 'fix-wordpress':
+    command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/',
+}
