34b774b1b70eca4a2f53cc73a1f37b41dc6755aa
ilyes01 <ilyesghrab7@gmail.com>
2021-11-06 23:15:20 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# Create a file in /tmp
+# Create a file in tmp
 file { '/tmp/holberton':
-  ensure  => present,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
