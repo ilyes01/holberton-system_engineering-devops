2cd4349ddb100389d14b55be3195878cce74ba5d
ilyes01 <ilyesghrab7@gmail.com>
2021-11-08 13:42:58 +0100

    fixxx that shit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
-""" Python script """
+"""
+script thatreturn information about the employee's todo list progress
+"""
 import requests
-import json
-from sys import argv
+import sys
 
+
+def fetch_api():
+    """ extract data from api """
+    user_i = int(sys.argv[1])
+    url_info = "https://jsonplaceholder.typicode.com/users/{}/".format(
+        user_i)
+    response = requests.get(url_info).json()
+    user_name = response.get("name")
+    url_todo = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        user_i)
+    todos = requests.get(url_todo).json()
+    number_completed_todos = 0
+    number_of_todos = 0
+    # completed_todos = []
+    task_list = ""
+    for todo in todos:
+        number_of_todos += 1
+        if todo.get("completed") is True:
+            number_completed_todos += 1
+            # completed_todos.append(todo["title"])
+            task_list += "\t " + todo.get("title") + "\n"
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name,
+        number_completed_todos,
+        number_of_todos
+        ))
+    print(task_list[:-1])
 
 if __name__ == "__main__":
-    employee_id = argv[1]
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/\
-' + employee_id).text
-    user = json.loads(user_response)
-
-    td_response = requests.get('https://jsonplaceholder.typicode.com/todos?\
-userId=' + employee_id).text
-    todos = json.loads(td_response)
-    completed = 0
-    tasks = []
-    for a_dict in todos:
-        if a_dict['completed'] is True:
-            completed += 1
-            tasks.append(a_dict['title'])
-
-    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-                                                          completed,
-                                                          len(todos)))
-    for title in tasks:
-        print("\t {}".format(title))
+    fetch_api()
