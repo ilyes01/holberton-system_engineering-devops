205bd6ac09ad75d46322859b567939097c039aff
ilyes01 <ilyesghrab7@gmail.com>
2021-11-16 22:24:17 +0100

    creat tmp file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# create a file
+# create a file /tmp.
 file { '/tmp/holberton':
-  ensure  => 'file',
-  content => 'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-}
+  content => 'I love Puppet'
+  }
