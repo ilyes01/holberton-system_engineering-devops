2777d1ef9c293d52968d290d6314c029a4b8069c
ilyes01 <ilyesghrab7@gmail.com>
2021-11-16 23:27:37 +0100

    commmmon bitch

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,50 +1,52 @@
 #!/usr/bin/python3
-""" emchyyyy plzzzzzzzzzzzzzzzz """
+"""
+    Recursive function that queries the Reddit API, parses the title of all hot
+    articles, and prints a sorted count of given keywords (case-insensitive,
+    delimited by spaces. Javascript should count as javascript, but java should
+    not).
+"""
+import re
 import requests
+headers = {'user-agent': 'ubuntu:hbtn:v1.0\
+ (by /u/Brandixitor)'}
 
 
-def count_words(subreedit, word_list, after="", d={}, check=0):
-    """Returns top 10 posts """
-    if check == 0:
-        for j in range(len(word_list)):
-            word_list[j] = word_list[j].lower()
-    d = dict.fromkeys(word_list)
-    url = 'https://www.reddit.com'
-    query_string = 'r/{}/hot.json'.format(subreedit)
-    s1 = "Mozilla/5.0 (Linux; Android 11; Pixel 2;"
-    s2 = "DuplexWeb-Google/1.0) AppleWebKit/537.36"
-    s3 = "(KHTML, like Gecko) Chrome/86.0.4240.193 Mobile Safari/537.36"
-    agent = s1 + s2 + s3
-    headers = {'User-agent': agent}
-    params = {'after': after}
-    r = requests.get(
-        '{}/{}'.format(url, query_string),
-        allow_redirects=False,
-        headers=headers,
-        params=params
-        )
-    if r.status_code == 403 or r.status_code == 404:
+def count_words(subreddit, word_list, after='', occurs={}):
+    """
+        Recursive function that queries the Reddit API, parses the title of all
+        hot articles, and prints a sorted count of given keywords
+        (case-insensitive, delimited by spaces.
+        Javascript should count as javascript, but java should not).
+    """
+    url = 'https://api.reddit.com/r/' + subreddit + '?limit=100&after=' + after
+    response = requests.get(url, headers=headers)
+    try:
+        data = response.json()
+    except:
         return
-    data = r.json()
-    after = data.get('data').get('after')
-    posts = data.get('data').get('children')
-    for post in posts:
-        title = post.get('data').get('title')
-        title_list = title.split(' ')
-        for j in range(len(title_list)):
-            title_list[j] = title_list[j].lower()
-        # (title_list)
-        # (word_list)
-        for word in title_list:
-            if word in word_list:
-                if d[word] is None:
-                    d[word] = 1
-                else:
-                    d[word] += 1
-    if after:
-        return count_words(subreedit, word_list, after, d, 1)
-    print(d)
-    return d
-
-if __name__ == '__main___':
-    count_words(subreedit)
+    if (str(response.status_code) == '404'):
+        return
+    dataLength = len(data['data']['children'])
+    if (dataLength is 0):
+        return
+    for i in range(0, dataLength):
+        try:
+            get_title = data['data']['children'][i]['data']['title']
+            for a in word_list:
+                try:
+                    occurs[a]
+                except KeyError:
+                    occurs[a] = 0
+                finally:
+                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
+                                         '', get_title)[1]
+        except:
+            pass
+    afterVal = data['data']['after']
+    if (afterVal is not None):
+        return count_words(subreddit, word_list, afterVal, occurs)
+    else:
+        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
+            if (occurs[key] > 0):
+                print("{}: {}".format(key, occurs[key]))
+        return
