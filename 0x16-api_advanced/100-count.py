cf7c549d18c69813cee639a613517cc4afb89fba
ilyes01 <ilyesghrab7@gmail.com>
2021-11-16 23:15:59 +0100

    counnt

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,48 +1,57 @@
 #!/usr/bin/python3
-"""
-    Recursive function that queries the Reddit API, parses the title of all hot
-    articles, and prints a sorted count of given keywords.
-"""
+'''
+ Recursive function that queries the Reddit API, parses the title of all hot
+ articles, and prints a sorted count of given keywords
+ '''
+import pprint
 import re
 import requests
-headers = {'user-agent': 'ubuntu:hbtn:v1.0\
- (by /u/Brandixitor)'}
+
+url = 'http://reddit.com/r/{}/hot.json'
 
 
-def count_words(subreddit, word_list, after='', occurs={}):
-    """
-        Recursive function that queries the Reddit API, parses the title of all
-        hot articles, and prints a sorted count of given keyword.
-    """
-    url = 'https://api.reddit.com/r/' + subreddit + '?limit=100&after=' + after
-    response = requests.get(url, headers=headers)
-    try:
-        data = response.json()
-    except:
-        return
-    if (str(response.status_code) == '404'):
-        return
-    dataLength = len(data['data']['children'])
-    if (dataLength is 0):
-        return
-    for i in range(0, dataLength):
-        try:
-            get_title = data['data']['children'][i]['data']['title']
-            for a in word_list:
-                try:
-                    occurs[a]
-                except KeyError:
-                    occurs[a] = 0
-                finally:
-                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
-                                         '', get_title)[1]
-        except:
-            pass
-    afterVal = data['data']['after']
-    if (afterVal is not None):
-        return count_words(subreddit, word_list, afterVal, occurs)
-    else:
-        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
-            if (occurs[key] > 0):
-                print("{}: {}".format(key, occurs[key]))
-        return
+def count_words(subreddit, word_list, hot_list=[], after=None):
+'''
+Recursive function that queries the Reddit API, parses the title of all
+hot articles, and prints a sorted count of given keywords
+'''
+    header = {'User-agent': 'tabbykatz-app3'}
+    params = {'limit': 100}
+    if isinstance(after, str):
+        if after != "DONE":
+            params['after'] = after
+        else:
+            return print_it(word_list, hot_list)
+
+    response = requests.get(url.format(subreddit),
+                            headers=header, params=params)
+    if response.status_code != 200:
+        return None
+    data = response.json().get('data', {})
+    after = data.get('after', 'DONE')
+    if not after:
+        after = "DONE"
+    hot_list = hot_list + [item.get('data', {}).get('title')
+                           for item in data.get('children', [])]
+    return count_words(subreddit, word_list, hot_list, after)
+
+
+def print_it(word_list, hot_list):
+    ''' we are printing this time '''
+    # print(hot_list)
+    # print(word_list)
+    count = {}
+    for word in word_list:
+        count[word] = 0
+    for title in hot_list:
+        for word in word_list:
+            count[word] = count[word] +\
+             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
+            # findall(thing, where, ignore case)
+
+    count = {k: v for k, v in count.items() if v > 0}
+    words = sorted(list(count.keys()))
+    for word in sorted(words,
+                       reverse=True, key=lambda k: count[k]):
+        # sorted( thing, descending, by count)
+        print("{}: {}".format(word, count[word]))
