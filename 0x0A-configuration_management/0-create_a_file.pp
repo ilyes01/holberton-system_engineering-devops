75f03c43d6fc30a7b42b46dc5aec75fe7881ab3a
ilyes01 <58910362+ilyes01@users.noreply.github.com>
2021-11-17 12:28:47 +0100

    Update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# create a file /tmp.
-file { '/tmp/holberton':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+# A script that creates a bew file called Holberton in tmp directory.
+  file { '/tmp/holberton':
+    ensure  => 'present',
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
   }
